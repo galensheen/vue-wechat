43cb05e5d7ff1561ee60f9473705e3e3617e92a2
galensheen <galensheen@aliyun.com>
2016-12-31 21:42:37 +0800

    reformat

--- src/mock/contact.js
+++ src/mock/contact.js
@@ -7,8 +7,8 @@ module.exports = [{
   "list": [{
     "id": 1,
     "name": "杨涛",
-    "nickname":"杨涛",
-    "remark":"阿涛",
+    "nickname": "杨涛",
+    "remark": "阿涛",
     "wxid": "wxid_yangtao",
     "qq": "00000",
     "email": "00000@qq.com",
@@ -17,13 +17,13 @@ module.exports = [{
     "qrCode": "",
     "signature": "个性签名"
   }]
-},{
+}, {
   "alpha": "B",
   "list": [{
     "id": 12,
     "name": "baijiahei",
-    "nickname":"baijiahei",
-    "remark":"白加黑",
+    "nickname": "baijiahei",
+    "remark": "白加黑",
     "wxid": "wxid_baijiahei",
     "qq": "00000",
     "email": "00000@qq.com",
@@ -36,33 +36,33 @@ module.exports = [{
   "alpha": "X",
   "list": [{
     "id": 11,
-    "name":"小明",
+    "name": "小明",
     "nickname": "小明",
-    "remark":"小明",
+    "remark": "小明",
     "wxid": "wxid_xiaoming",
     "qq": "00011",
     "email": "00011@qq.com",
     "type": "friends",
     "iconSrc": "//images2015.cnblogs.com/blog/528573/201609/528573-20160925131419593-1613633261.jpg",
     "qrCode": "",
-    "telephone":18812345678,
+    "telephone": 18812345678,
     "signature": "个性签名",
-    "area":["中国","北京","海淀"],
-    "selfPhotos":[{imgSrc:""}]
-  },{
+    "area": ["中国", "北京", "海淀"],
+    "selfPhotos": [{imgSrc: ""}]
+  }, {
     "id": 13,
     "nickname": "小亮",
     "name": "小亮",
-    "remark":"小亮",
+    "remark": "小亮",
     "wxid": "wxid_xiaoliang",
     "qq": "00011",
     "email": "00011@qq.com",
     "type": "friends",
     "iconSrc": "//images2015.cnblogs.com/blog/528573/201609/528573-20160922181544949-1515459394.jpg",
     "qrCode": "",
-    "telephone":18812345678,
+    "telephone": 18812345678,
     "signature": "个性签名",
-    "area":["中国","北京","海淀"],
-    "selfPhotos":[{imgSrc:""}]
+    "area": ["中国", "北京", "海淀"],
+    "selfPhotos": [{imgSrc: ""}]
   }]
 }];
